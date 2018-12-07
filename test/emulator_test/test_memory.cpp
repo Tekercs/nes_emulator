e71b43c820c55241b5c0132240c0e57fbde48036
Sebestyén Bence <bence.sebestyen.2015@uni.strath.ac.uk>
2018-12-07 12:36:44 +0000

    added test for setting then accessing memory location

--- test/emulator_test/test_memory.cpp
+++ test/emulator_test/test_memory.cpp
@@ -16,5 +16,26 @@ SCENARIO("when reading memory", "[Memory]")
                 REQUIRE(value == 0);
             }
         }
+        
+        delete memory;
+    }
+    
+    GIVEN("a memory with value 12 at 0x0012")
+    {
+        const uint16_t ADDRESS = 0x0012;
+        const uint8_t VALUE = 12;
+        
+        Emulator::Memory* mem = new Emulator::Memory();
+        mem->setMemoryAt(ADDRESS, VALUE);
+        
+        WHEN("memory accessed at 0x0012")
+        {
+            uint8_t value = mem->getMemoryFrom(ADDRESS);
+            
+            THEN("value 12 returned")
+            {
+                REQUIRE(value == VALUE);
+            }
+        }
     }
 }
