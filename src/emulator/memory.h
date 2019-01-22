bc0c772ce1e63fedbe738f604916c7de641720c7
Sebestyén Bence <bence.sebestyen.2015@uni.strath.ac.uk>
2019-01-22 23:31:43 +0000

    some doc for init

--- src/emulator/memory.h
+++ src/emulator/memory.h
@@ -17,8 +17,23 @@
 #define PRG_LOWER           0x8000
 #define PRG_UPPER           0xC000
 
+/**
+ * @namespace Emulator
+ *
+ * @brief This namespace groups emulator classes.
+ */
 namespace Emulator
 {
+
+    /**
+     * @class Memory
+     *
+     * @brief NES Memory module representation.
+     *
+     * @details This class represents the NES' Memory
+     * module including it's mirroring properties
+     * based on the specification.
+     */
     class Memory
     {
     private:
@@ -28,8 +43,26 @@ namespace Emulator
         void generateIORegisters();
 
     public:
+
+        /**
+         * @brief Constructor for Memory object
+         *
+         * @details Creates an empty Memory emulation object
+         * with preconfigured memory mirroring based on the
+         * NES system specification.
+         */
         Memory();
 
+        /**
+         * @brief Set accessor function for memory slots.
+         *
+         * @param address for the target memory location
+         * @param value for the given address to write
+         *
+         * @details Write the passed 8 bit long unsigned integer
+         * value to the given memory address which represented
+         * with 16 bit unsigned integer value.
+         */
         void setAt(uint16_t address, uint8_t value);
         uint8_t getFrom(uint16_t address);
     };
