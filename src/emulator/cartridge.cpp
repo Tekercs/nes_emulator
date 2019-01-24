dfeae9f20c19b2b6a9f8a244f2d713379d8557dc
Sebestyén Bence <bence.sebestyen.2015@uni.strath.ac.uk>
2019-01-24 23:08:33 +0000

    reading prg rom from binary file

--- src/emulator/cartridge.cpp
+++ src/emulator/cartridge.cpp
@@ -19,6 +19,7 @@ Emulator::Cartridge::Cartridge(const std::string &path)
         ++iterator;
     }
 
+    // read the trainer
     bool isTrainerPresented = ((header[6] & 0x04 ) > 0);
     if (isTrainerPresented)
     {
@@ -28,5 +29,21 @@ Emulator::Cartridge::Cartridge(const std::string &path)
             i = *iterator;
             ++iterator;
         }
+    }
+
+    // read the PRG rom size
+    uint16_t prgRomSize16KBBatch = 0;
+    prgRomSize16KBBatch = header[9] & 0b00001111; // first 4 bit from header byte 9 as MSB
+    prgRomSize16KBBatch = prgRomSize16KBBatch << 8;
+    prgRomSize16KBBatch += header[4]; // the while header byte 6 as LSB
+
+    int prgRomSize = 16384 * prgRomSize16KBBatch;
+
+    // read the actual PRG rom content
+    uint8_t prgRom[prgRomSize];
+    for (uint8_t &i : prgRom)
+    {
+        i = *iterator;
+        ++iterator;
     }
 }
