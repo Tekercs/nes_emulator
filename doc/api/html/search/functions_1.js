bd57e3572154393354d00c68902606d5c2528e3a
Sebestyén Bence <bence.sebestyen.2015@uni.strath.ac.uk>
2019-02-12 12:21:45 +0000

    api doc for new accessor function

--- doc/api/html/search/functions_1.js
+++ doc/api/html/search/functions_1.js
@@ -6,5 +6,6 @@ var searchData=
   ['getfrom',['getFrom',['../classEmulator_1_1Memory_1_1Memory.html#acbbbd652d281541c037b994de98b241c',1,'Emulator::Memory::Memory']]],
   ['getmiscrom',['getMiscRom',['../classEmulator_1_1ROM_1_1Cartridge.html#a0c6530e6da9fc8f4de833c85b84b564b',1,'Emulator::ROM::Cartridge']]],
   ['getprgrom',['getPrgRom',['../classEmulator_1_1ROM_1_1Cartridge.html#ac2a218bb948eb1290f5ce41e4906cbf0',1,'Emulator::ROM::Cartridge']]],
-  ['getrawheader',['getRawHeader',['../classEmulator_1_1ROM_1_1Cartridge.html#a17c76c7f76528da01d58348e955ddb44',1,'Emulator::ROM::Cartridge']]]
+  ['getrawheader',['getRawHeader',['../classEmulator_1_1ROM_1_1Cartridge.html#a17c76c7f76528da01d58348e955ddb44',1,'Emulator::ROM::Cartridge']]],
+  ['gettrainer',['getTrainer',['../classEmulator_1_1ROM_1_1Cartridge.html#a1c3e8eb8b986e1fe761f78e3808e8323',1,'Emulator::ROM::Cartridge']]]
 ];
