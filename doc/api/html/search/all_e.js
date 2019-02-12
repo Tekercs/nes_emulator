d0e88607023bc63b99917817f94b140b3cf14d60
Sebestyén Bence <bence.sebestyen.2015@uni.strath.ac.uk>
2019-02-12 13:02:56 +0000

    trainers exists accessor function api doc

--- doc/api/html/search/all_e.js
+++ doc/api/html/search/all_e.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['trainer',['trainer',['../classEmulator_1_1ROM_1_1Cartridge.html#a4e21670c5796d10df7a24de6a24b3dc1',1,'Emulator::ROM::Cartridge']]],
-  ['trainer_5flength',['TRAINER_LENGTH',['../cartridge_8h.html#a3cc4edbcbfe141075a386e8d63c69ba4',1,'cartridge.h']]]
+  ['trainer_5flength',['TRAINER_LENGTH',['../cartridge_8h.html#a3cc4edbcbfe141075a386e8d63c69ba4',1,'cartridge.h']]],
+  ['trainerexists',['trainerExists',['../classEmulator_1_1ROM_1_1Cartridge.html#a4faa5d41b05ca6502736cae3ec1c3a17',1,'Emulator::ROM::Cartridge']]]
 ];
---
+++ doc/api/html/search/functions_6.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['trainerexists',['trainerExists',['../classEmulator_1_1ROM_1_1Cartridge.html#a4faa5d41b05ca6502736cae3ec1c3a17',1,'Emulator::ROM::Cartridge']]]
+];
