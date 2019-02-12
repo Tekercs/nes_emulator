46ed48ea831ce9577f487c653eaf5c984f9c31c4
Sebestyén Bence <bence.sebestyen.2015@uni.strath.ac.uk>
2019-02-12 12:48:53 +0000

    misc rom size field and accessor apidoc for Cartridge class

--- doc/api/html/search/all_5.js
+++ doc/api/html/search/all_5.js
@@ -5,6 +5,7 @@ var searchData=
   ['getchrrom',['getChrRom',['../classEmulator_1_1ROM_1_1Cartridge.html#ac9074dacf9b7e40cf5e88e38c14eae81',1,'Emulator::ROM::Cartridge']]],
   ['getfrom',['getFrom',['../classEmulator_1_1Memory_1_1Memory.html#acbbbd652d281541c037b994de98b241c',1,'Emulator::Memory::Memory']]],
   ['getmiscrom',['getMiscRom',['../classEmulator_1_1ROM_1_1Cartridge.html#a0c6530e6da9fc8f4de833c85b84b564b',1,'Emulator::ROM::Cartridge']]],
+  ['getmiscromsize',['getMiscRomSize',['../classEmulator_1_1ROM_1_1Cartridge.html#ab7b20b534f67e55c473ee0e66ed85ddb',1,'Emulator::ROM::Cartridge']]],
   ['getprgrom',['getPrgRom',['../classEmulator_1_1ROM_1_1Cartridge.html#ac2a218bb948eb1290f5ce41e4906cbf0',1,'Emulator::ROM::Cartridge']]],
   ['getrawheader',['getRawHeader',['../classEmulator_1_1ROM_1_1Cartridge.html#a17c76c7f76528da01d58348e955ddb44',1,'Emulator::ROM::Cartridge']]],
   ['gettrainer',['getTrainer',['../classEmulator_1_1ROM_1_1Cartridge.html#a1c3e8eb8b986e1fe761f78e3808e8323',1,'Emulator::ROM::Cartridge']]]
