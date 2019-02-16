851f24d41db685da58a88d47329fbb5e790dba39
Sebestyén Bence <bence.sebestyen.2015@uni.strath.ac.uk>
2019-02-16 22:28:46 +0000

    given starting value for the fields

--- src/emulator/cpu.cpp
+++ src/emulator/cpu.cpp
@@ -2,5 +2,10 @@
 
 Emulator::Cpu::Cpu::Cpu()
 {
-
+    this->accumulator = 0;
+    this->indexRegisterX = 0;
+    this->indexRegisterY = 0;
+    this->processorStatus = 0;
+    this->programCounter = 0;
+    this->stackPointerOffset = 0;
 }
