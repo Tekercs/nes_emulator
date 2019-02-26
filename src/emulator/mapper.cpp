a5faa9d976f24fc66b56a41a65734daa95fd5d70
Sebestyén Bence <bence.sebestyen.2015@uni.strath.ac.uk>
2019-02-26 17:06:13 +0000

    abstract class for mapper

--- src/emulator/mapper.cpp
+++ src/emulator/mapper.cpp
@@ -1,1 +1,23 @@
 #include "mapper.h"
+
+#include <memory>
+
+using namespace std;
+using namespace Emulator::Cpu;
+using namespace Emulator::Memory;
+using namespace Emulator::ROM;
+
+Mapper::Mapper(const Cartridge &cartridge
+        , const Emulator::Memory::Memory &memory
+        , const Emulator::Cpu::Cpu &cpu)
+: cpu(cpu)
+, cartridge(cartridge)
+, memory(memory)
+{ }
+
+shared_ptr<Mapper> createMapper(const Cartridge &cartridge
+        , const Memory &memory
+        , const Cpu &cpu)
+{
+    return shared_ptr<Mapper>();
+}
--- src/emulator/mapper.h
+++ src/emulator/mapper.h
@@ -1,9 +1,26 @@
 #pragma once
 
+#include <memory>
+
+#include <cartridge.h>
+#include <memory.h>
+#include <cpu.h>
+
 namespace Emulator::ROM
 {
     class Mapper
     {
+    private:
+        const Cartridge& cartridge;
+        const Memory::Memory& memory;
+        const Cpu::Cpu& cpu;
+
+    public:
+        Mapper(const Cartridge& cartridge, const Memory::Memory& memory,  const Cpu::Cpu& cpu);
+
+        virtual void map() = 0;
 
     };
+
+    std::shared_ptr<Mapper> createMapper(const Cartridge& cartridge, const Memory::Memory& memory,  const Cpu::Cpu& cpu);
 }
