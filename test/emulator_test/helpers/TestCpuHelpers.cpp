88633ce814ce40506bb7ee6202c97ab4ed383e58
Sebestyén Bence <bence.sebestyen.2015@uni.strath.ac.uk>
2019-03-01 00:32:51 +0000

    cpu nestest.nes init

---
+++ test/emulator_test/helpers/TestCpuHelpers.cpp
@@ -0,0 +1,10 @@
+#include "TestCpuHelpers.h"
+
+using namespace std;
+using namespace Emulator::Memory;
+using namespace Emulator::Cpu;
+
+std::string parseCurrentStateLog(const Emulator::Memory::Memory& memory, const Emulator::Cpu::Registers& registers)
+{
+    return "";
+}
---
+++ test/emulator_test/test_cpu.cpp
@@ -0,0 +1,52 @@
+#include <catch.hpp>
+#include <memory>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+#include <Memory/Memory.h>
+#include <Rom/Cartridge.h>
+#include <Rom/Mapper.h>
+#include <Cpu/Cpu.h>
+#include <Cpu/Registers.h>
+
+#include "helpers/TestCpuHelpers.h"
+
+using namespace std;
+using namespace Emulator::Cpu;
+using namespace Emulator::Memory;
+using namespace Emulator::ROM;
+
+#define PATH_NESTEST "../../../test/emulator_test/test_roms/kevtris/nestest.nes"
+#define PATH_NESTEST_LOG "../../../test/emulator_test/test_roms/kevtris/nestest.log"
+
+SCENARIO("nestest.nes by kevtris")
+{
+    GIVEN("parsed and mapped nestest.nes and CPU PC at 0xC000")
+    {
+        shared_ptr<Memory> memory = make_shared<Memory>();
+        shared_ptr<Registers> registers = make_shared<Registers>();
+        registers->setProgramCounter(0xC000);
+
+        Cpu cpu(memory, registers);
+
+        Cartridge cartridge(PATH_NESTEST);
+        auto mapper = createMapper(cartridge, *memory.get(), cpu);
+        mapper->map();
+
+        ifstream logFile(PATH_NESTEST_LOG);
+
+        THEN("Running and checking same output after every cycle")
+        {
+            string logLine;
+            string currentState;
+            while(getline(logFile, logLine))
+            {
+                currentState = parseCurrentStateLog(*memory.get(), *registers.get());
+                REQUIRE(logLine == currentState);
+
+                ++cpu;
+            }
+        }
+    }
+}
