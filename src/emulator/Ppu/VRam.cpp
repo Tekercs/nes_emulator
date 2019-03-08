90835705fc1bbba43071f3fdecaf7a3649faa9d7
Sebestyén Bence <bence.sebestyen.2015@uni.strath.ac.uk>
2019-03-08 21:07:04 +0000

    nametable mirroring

--- src/emulator/Ppu/VRam.cpp
+++ src/emulator/Ppu/VRam.cpp
@@ -8,6 +8,18 @@ using namespace Emulator::Ppu;
 
 VRam::VRam()
 {
-    generate(begin(this->memory), end(this->memory), []() { return 0; });
+    generate(begin(this->memory), end(this->memory), []() { return new uint8_t(0); });
     generate(begin(this->oam), end(this->oam), []() { return 0; });
+
+    this->generateNametable();
+}
+
+void VRam::generateNametable()
+{
+    uint8_t* nametables[NAME_SIZE * 4] = {};
+    generate(begin(nametables), end(nametables), []() { return new uint8_t(0); });
+
+    copy(begin(nametables), end(nametables), begin(this->memory) + NAME_0);
+    copy(begin(nametables), end(nametables), begin(this->memory) + NAME_MIRRORING_STARTS);
+
 }
--- src/emulator/Ppu/VRam.h
+++ src/emulator/Ppu/VRam.h
@@ -5,13 +5,27 @@
 #define VRAM_SIZE 0x10000
 #define OAM_SIZE  0x10000
 
+#define PATTERN_0 0x0000
+#define PATTERN_1 0x1000
+#define PATTERN_SIZE 0x1000
+
+#define NAME_0 0x2000
+#define NAME_1 0x2400
+#define NAME_2 0x2400
+#define NAME_3 0x2C00
+
+#define NAME_SIZE 0x0400
+#define NAME_MIRRORING_STARTS 0x3000
+
 namespace Emulator::Ppu
 {
     class VRam
     {
     private:
-        uint8_t memory[VRAM_SIZE];
+        uint8_t* memory[VRAM_SIZE];
         uint8_t oam[OAM_SIZE];
+
+        void generateNametable();
 
     public: 
         VRam();
