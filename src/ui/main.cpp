1da4fc926528cdacfb6e29983195c0e86c5974fc
Sebestyén Bence <bence.sebestyen.2015@uni.strath.ac.uk>
2019-03-08 18:11:48 +0000

    just render a rectangle

--- src/ui/main.cpp
+++ src/ui/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <SDL2/SDL.h>
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window* window = SDL_CreateWindow("first sdl", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 400, 400, SDL_WINDOW_SHOWN);
+    SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, 0);
+
+    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+
+    SDL_Rect rect = {.x = 0, .y = 0, .w = 100, .h = 200};
+
+    SDL_RenderFillRect(renderer, &rect);
+
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(5000);
+
     return 0;
 }
