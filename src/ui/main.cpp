073950ffc6857b446681d1a480e2a6ae731a38a2
Sebestyén Bence <bence.sebestyen.2015@uni.strath.ac.uk>
2019-03-08 18:33:59 +0000

    render loop + polling user vents closes #46

--- src/ui/main.cpp
+++ src/ui/main.cpp
@@ -4,18 +4,28 @@
 int main()
 {
     SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window* window = SDL_CreateWindow("first sdl", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 400, 400, SDL_WINDOW_SHOWN);
-    SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, 0);
-
-    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+    auto window = SDL_CreateWindow("first sdl", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 400, 400, SDL_WINDOW_SHOWN);
+    auto renderer = SDL_CreateRenderer(window, -1, 0);
 
     SDL_Rect rect = {.x = 0, .y = 0, .w = 100, .h = 200};
-
-    SDL_RenderFillRect(renderer, &rect);
-
     SDL_RenderPresent(renderer);
 
-    SDL_Delay(5000);
+    SDL_Event event;
+    auto quit = false;
+    do
+    {
+        SDL_PollEvent(&event);
+
+        quit = (event.type == SDL_QUIT);
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+        SDL_RenderClear(renderer);
+
+        SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+        SDL_RenderFillRect(renderer, &rect);
+
+        SDL_RenderPresent(renderer);
+    } while (!quit);
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
