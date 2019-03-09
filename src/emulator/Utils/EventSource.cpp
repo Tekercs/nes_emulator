1cbcddd03b18952a9634bf71eeabe10c5d8c950a
Sebestyén Bence <bence.sebestyen.2015@uni.strath.ac.uk>
2019-03-09 19:50:09 +0000

    dummy event sorouce calss

--- src/emulator/Utils/EventSource.cpp
+++ src/emulator/Utils/EventSource.cpp
@@ -1,1 +1,21 @@
 #include "EventSource.h"
+
+using namespace std;
+using namespace Emulator::Utils;
+
+
+EventSource& EventSource::operator += (Listener* listener)
+{
+    return *this;
+}
+
+EventSource& EventSource::operator -= (Listener* listener)
+{
+    return *this;
+}
+
+template <class T>
+void EventSource::operator() (std::initializer_list<T> args)
+{
+
+}
--- src/emulator/Utils/EventSource.h
+++ src/emulator/Utils/EventSource.h
@@ -1,9 +1,22 @@
 #pragma once
 
+#include <set>
+#include <initializer_list>
+
+#include <Utils/Listener.h>
+
 namespace Emulator::Utils
 {
     class EventSource 
     {
-        
+    private:
+        std::set<Listener*> listeners;
+
+    public:
+        EventSource& operator += (Listener* listener);
+        EventSource& operator -= (Listener* listener);
+
+        template<class T>
+        void operator() (std::initializer_list<T> args);
     };
 }
