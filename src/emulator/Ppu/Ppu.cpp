48785f2d524bd1d8d9b8b66b52624a82b95af7df
Sebestyén Bence <bence.sebestyen.2015@uni.strath.ac.uk>
2019-03-11 17:18:21 +0000

    writing to OAM one by one through CPU

--- src/emulator/Ppu/Ppu.cpp
+++ src/emulator/Ppu/Ppu.cpp
@@ -1,9 +1,10 @@
 #include "Ppu.h"
 
-#include <iostream>
+#include <Utils/Converters.h>
 
 using namespace std;
 using namespace Emulator::Ppu;
+using namespace Emulator::Utils;
 
 Ppu::Ppu(std::shared_ptr<VRam> vram, std::shared_ptr<Emulator::Memory::Memory> memory)
 : memory(std::move(memory))
@@ -17,6 +18,16 @@ Ppu::Ppu(std::shared_ptr<VRam> vram, std::shared_ptr<Emulator::Memory::Memory> m
 
 void Ppu::notify(initializer_list<string> parameters)
 {
-    auto param = parameters.begin();
-    cout << param << endl;
+    if (*parameters.begin() == "memwrite")
+        if ((*(parameters.begin() +1)) == "2003")
+            this->oamAccessor.adress = static_cast<uint8_t>(convertHexStringToInt(*(parameters.begin() + 2)));
+
+
+    if (*parameters.begin() == "memwrite")
+        if ((*(parameters.begin() +1)) == "2004")
+        {
+            this->oamAccessor.value = static_cast<uint8_t>(convertHexStringToInt(*(parameters.begin() + 2)));
+            this->vram->writeOAM(this->oamAccessor.adress, this->oamAccessor.value);
+            auto laci = "breakpoint";
+        }
 }
