f61a97f80507386383536837dbe231ffca69a56d
Sebestyén Bence <bence.sebestyen.2015@uni.strath.ac.uk>
2019-03-14 16:14:28 +0000

    gamewindow class instance

---
+++ src/ui/GameWindow.cpp
@@ -0,0 +1,20 @@
+//
+// Created by bence on 14/03/19.
+//
+
+#include "GameWindow.h"
+
+Ui::GameWindow::GameWindow(uint8_t scaling)
+{
+
+}
+
+void Ui::GameWindow::colorPixel(Ui::Cords cords, Ui::Color color)
+{
+
+}
+
+void Ui::GameWindow::clearScreen()
+{
+
+}
---
+++ src/ui/GameWindow.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <SDL2/SDL.h>
+
+#define MAX_HORIZONTAL 256
+#define MAX_VERTICAL   240
+
+namespace Ui
+{
+    struct Cords
+    {
+        uint16_t horizontal;
+        uint16_t vertical;
+    };
+
+    struct Color
+    {
+        uint8_t red;
+        uint8_t green;
+        uint8_t blue;
+    };
+
+    class GameWindow
+    {
+    private:
+        SDL_Window* window;
+        SDL_Renderer* renderer;
+
+    public:
+        explicit GameWindow(uint8_t scaling);
+
+        void colorPixel(Cords cords, Color color);
+        void clearScreen();
+    };
+}
